5c262ffad643b831e5d462d1c2a4537b2638ff24
owolfdev <owolfdev@gmail.com>
2023-07-05 05:32:14 +0700

    init

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -4,15 +4,17 @@ const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 export async function POST(req: NextRequest) {
   try {
-    const { email, project } = await req.json();
+    // const { email, project } = await req.json();
 
-    console.log("email:", email);
-    console.log("project:", project);
+    // console.log("email:", email);
+    // console.log("project:", project);
 
-    const notionData = await notion.databases.query({
-      database_id: project,
-    });
+    // const notionData = await notion.databases.query({
+    //   database_id: project,
+    // });
 
+    const email = "someone@g.com";
+    const project = "123456789";
     // you can now use the `email` and `project` values from the request body
     const data = {
       name: "John Dolly",
